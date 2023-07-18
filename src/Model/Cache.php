62f8e7e1ffc0feff02d3b35a5a46703a3715f610
mobbexLuis <luis.scarrone@mobbex.com>
2023-07-18 17:48:09 -0300

    update namespace add model methods

---
+++ src/Model/Cache.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Mobbex\Model;
+
+class Cache
+{
+    /**
+     * Generates a unique key for values to store.
+     * 
+     * @param string $key Initial name for de key.
+     * @param array  $params A list of values to add hashed to the key.
+     * 
+     * @return string
+     * 
+     */
+    public static function generateKey($key, ...$params)
+    {
+        return $key . md5(\Mobbex\Platform::$settings['api_key'] . \Mobbex\Platform::$settings['access_token'] . implode($params));
+    }
+
+    /**
+     * Store data in mobbex cache table.
+     * 
+     * @param string $key Identifier key for data to store.
+     * @param string $data Data to store.
+     * 
+     * @return boolean
+     * 
+     */
+    public function store($key, $data)
+    {
+        return isset($key) && isset($data) ? true : null;
+    }
+
+    /**
+     * Get data stored in mobbex chache table.
+     * 
+     * @param string $key Identifier key for cache data.
+     * 
+     * @return string|bool $data Data to store.
+     * 
+     */
+    public function get($key)
+    {
+        return isset($key) ? true : null;
+    }
+
+    /**
+     * Delete expired stored data in cache table.
+     */
+    public function deleteExpiredCache()
+    {
+        return null;
+    }
+}
