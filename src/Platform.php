ebf6d39b80970666ede32ab8e021479c97927c14
EMunozMobbex <e.munoz.chedel@mobbex.com>
2023-10-26 17:15:31 -0300

    Changes requested by @jlmobbex

--- src/Platform.php
+++ src/Platform.php
@@ -38,7 +38,7 @@ final class Platform
         'emit_customer_success_notification' => true,
         'emit_customer_failure_notification' => true,
         'emit_customer_waiting_notification' => true,
-        'embedVersion'                       => true,
+        'embed_version'                      => '1.1.0',
     ];
 
     /** @var \Mobbex\Model\Cache */
