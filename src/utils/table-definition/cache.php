64d22087681920c5af5fe7a86f1a39589972d9c4
EMunozMobbex <e.munoz.chedel@mobbex.com>
2024-04-17 17:16:28 -0300

    Change cache column type.

--- src/utils/table-definition/cache.php
+++ src/utils/table-definition/cache.php
@@ -8,6 +8,6 @@
  */
 return [
     ['Field' => 'cache_key', 'Type' => 'varchar(255)', 'Null' => 'NO', 'Key' => 'PRI', 'Default' => null,                  'Extra' => ''],
-    ['Field' => 'data',      'Type' => 'text',         'Null' => 'NO', 'Key' => '',    'Default' => null,                  'Extra' => ''],
+    ['Field' => 'data',      'Type' => 'longtext',     'Null' => 'NO', 'Key' => '',    'Default' => null,                  'Extra' => ''],
     ['Field' => 'date',      'Type' => 'timestamp',    'Null' => 'NO', 'Key' => '',    'Default' => 'current_timestamp()', 'Extra' => ''],
 ];
