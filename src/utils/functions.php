fb3750e8cb5948fad4a57f68a744215190781237
jlmobbex <javier.lopez@mobbex.com>
2024-05-30 05:14:40 -0300

    Add functions to sort arrays using index maps and paths

---
+++ src/utils/functions.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Mobbex;
+
+/**
+ * Sort an multidimensional array using an index map as guide.
+ * 
+ * @param array &$array The array to sort.
+ * @param array $indexes The index map.
+ * @param string $indexPath The position to find the index in first array. Use dots on multidimensional arrays.
+ * 
+ * @see \Mobbex\arrayAt() to more info on paths.
+ */
+function multisortByIndex(&$array, $indexes, $indexPath) {
+    if (!$array || !$indexes)
+        return;
+
+    usort($array, function($a, $b) use($indexes, $indexPath) {
+        $indexA = array_search(arrayAt($a, $indexPath), $indexes);
+        $indexB = array_search(arrayAt($b, $indexPath), $indexes);
+
+        // If an index is not found, consider it infinite
+        if ($indexA === false) $indexA = PHP_INT_MAX;
+        if ($indexB === false) $indexB = PHP_INT_MAX;
+
+        return $indexA - $indexB;
+    });
+}
+
+/**
+ * Access to an array using a path (dots for multidimensional access).
+ * 
+ * @param array $array The array to access.
+ * @param string $path The path with the positions to access into the array.
+ * 
+ * @return mixed The array value obtained.
+ */
+function arrayAt($array, $path) {
+    $currentPos = $array;
+
+    foreach (explode('.', $path) as $value) {
+        if (!array_key_exists($value, $currentPos))
+            return null;
+
+        $currentPos = $currentPos[$value];
+    }
+
+    return $currentPos;
+  }
